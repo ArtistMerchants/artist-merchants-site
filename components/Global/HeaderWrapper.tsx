65a127da00bc56ee7feb4de67ce0d805dcc6348f
Logan Sparlin <logan.sparlin@gmail.com>
2024-06-30 12:07:17 -0400

    Header

--- components/Global/HeaderWrapper.tsx
+++ components/Global/HeaderWrapper.tsx
@@ -13,15 +13,8 @@ export const HeaderWrapper = ({ children, relative = false }) => {
         relative
           ? 'relative pt-20 md:absolute md:pt-0'
           : 'absolute left-0 top-20'
-      } z-[2] flex w-full items-start justify-between gap-20 text-12 leading-120 md:top-32 md:block md:text-14`}
+      } z-[2] flex w-full items-start justify-between gap-20 text-14 leading-120 md:top-32 md:block md:text-14`}
     >
-      <Link
-        href="/"
-        className="fixed left-20 top-20 inline-block md:hidden"
-        onClick={() => setMenuOpen(false)}
-      >
-        <Logo className="h-auto w-40 md:w-52" />
-      </Link>
       <Link
         href="/"
         className="left-20 top-20 inline-block opacity-0 md:hidden"
@@ -31,9 +24,6 @@ export const HeaderWrapper = ({ children, relative = false }) => {
         <Logo className="h-auto w-40 md:w-52" />
       </Link>
       {children}
-      <div className="fixed right-20 top-20 md:hidden">
-        <MenuButton />
-      </div>
       <div className="opacity-0 md:hidden" aria-hidden>
         <MenuButton />
       </div>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,7 @@ import { useSiteStore } from 'hooks/useSiteStore'
 import { usePathname } from 'next/navigation'
 import { AnimatePresence, motion } from 'framer-motion'
 import { Logo } from './Global/Logo'
+import { MenuButton } from './Global/MenuButton'
 import { FC } from 'react'
 import Link from 'next/link'
 
@@ -37,6 +38,16 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
         key={pageKey}
         className="ease relative z-[1] min-h-screen bg-white px-20 text-black transition-colors duration-1000 dark:bg-black dark:text-white md:grid md:grid-cols-9 md:px-32"
       >
+        <Link
+          href="/"
+          className="fixed left-20 top-20 z-[100] inline-block md:hidden"
+          onClick={() => setMenuOpen(false)}
+        >
+          <Logo className="h-auto w-40 md:w-52" />
+        </Link>
+        <div className="fixed right-20 top-0 z-[100] py-20 text-14 leading-100 md:hidden">
+          <MenuButton />
+        </div>
         <Link
           href="/"
           className="hidden py-20 md:col-span-1 md:inline-block md:py-32"
