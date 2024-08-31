775f62c541787b8b8df3e8f29f7d072e17204231
Logan Sparlin <logan.sparlin@gmail.com>
2024-08-30 22:47:56 -0400

    Studio updates

--- sanity.config.tsx
+++ sanity.config.tsx
@@ -1,3 +1,5 @@
+import { ArrowRightIcon } from '@sanity/icons'
+import { Flex, Button } from '@sanity/ui'
 import { visionTool } from '@sanity/vision'
 import { apiVersion, dataset, projectId } from './lib/sanity.api'
 import { structurePlugin, structureConfig } from './plugins/structure'
@@ -13,12 +15,39 @@ import materialCategory from './schemas/pages/materialCategory'
 
 const title = 'Artist Merchants'
 
+const MyCustomTool = (props) => {
+  const { renderDefault } = props
+  return (
+    <Flex align="center" gap={2}>
+      {renderDefault(props)}
+      <Button
+        as="a"
+        href="https://artist-merchants.vercel.app/"
+        target="_blank"
+        text="View Live Site"
+        iconRight={ArrowRightIcon}
+        mode="bleed"
+        selected={true}
+        radius={2}
+        fontSize={1}
+        padding={2}
+        space={3}
+      ></Button>
+    </Flex>
+  )
+}
+
 export default defineConfig({
   projectId,
   dataset,
   basePath: '/studio',
   title,
   icon: StudioIcon,
+  studio: {
+    components: {
+      toolMenu: MyCustomTool,
+    },
+  },
   schema: {
     types: [settings, homePage, project, projectCategory, materialCategory],
   },
