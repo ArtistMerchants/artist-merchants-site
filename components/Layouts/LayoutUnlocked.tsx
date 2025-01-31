e349eb286d04b2956c4e8dc71a2c17de1c6c917e
Logan Sparlin <logan.sparlin@gmail.com>
2025-01-31 17:20:29 -0500

    Add header logo unlocked

--- components/Layouts/LayoutUnlocked.tsx
+++ components/Layouts/LayoutUnlocked.tsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion'
 import { FC } from 'react'
 import { SiteMeta } from '../SiteMeta'
 import { HeaderUnlocked } from '../Header/HeaderUnlocked'
+import Link from 'next/link'
+import { Logo } from 'components/Global/Logo'
 
 interface LayoutUnlockedProps {
   children: React.ReactNode
@@ -17,6 +19,12 @@ export const LayoutUnlocked: FC<LayoutUnlockedProps> = ({
   return (
     <div className="min-h-screen gap-y-60 text-body md:gap-y-0">
       <div className="site-grid w-full">
+        <Link
+          href="/"
+          className="absolute left-0 top-20 col-span-1 block pl-20 md:relative md:top-0"
+        >
+          <Logo className="h-auto w-full max-w-38 md:max-w-50" />
+        </Link>
         <HeaderUnlocked {...settings} />
       </div>
       <SiteMeta {...settings} />
